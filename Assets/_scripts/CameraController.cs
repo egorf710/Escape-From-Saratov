79e844376fd9ab00fb48da8019865ae3e66c1398
Artem <99097444+darkb0x@users.noreply.github.com>
2022-03-01 20:48:28 +0200

    Следование камеры
    -Создал скрипт CameraController. Он висит на камере и отвечает за её передвижением.
    -Скрипт имеет параметр followSpeed который отвечает с какой скоростью будет происходить движение камеры

---
+++ Assets/_scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform _target;
+    Vector3 _targetInVector3; // нужно чтобы позиция в координатах Z не менялась у камеры
+
+    [Header("Скорость с какой камера будет приследость игрока(_target)")]
+    [SerializeField, Range(1,10)] private float followSpeed = 3;
+
+    private void Start()
+    {
+        _target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    private void FixedUpdate()
+    {
+        _targetInVector3 = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, _targetInVector3, followSpeed * Time.deltaTime);
+    }
+}
