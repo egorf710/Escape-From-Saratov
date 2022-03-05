466c111d1953873f251fe96f42237d6bc5b6f836
Artem <99097444+darkb0x@users.noreply.github.com>
2022-03-05 19:46:06 +0200

    ФОРТНАЙТ ИЛИ ПАБДЖИ??

--- Assets/Ckpiptes.cs
+++ Assets/Ckpiptes.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Ckpiptes : MonoBehaviour
 {
     //осярни яйпхор охьх ячдю ябнх кчани лернд
+    private string PiskaVJopa()
+    {
+        Debug.Log("darkbox from italy");
+        return "not planed kid";
+    }
 }
